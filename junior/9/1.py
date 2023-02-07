b5e58a50f98abb3f74b4758f5d13ef986b2ed6da
Elena Nikolaevskaya <elena.nikolaevskaya@cosysoft.ru>
2023-02-07 18:23:36 +0400

    junior-9.1

---
+++ junior/9/1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/sample/")
+def sample(offer_ids: str) -> dict:
+    """Sample random offer"""
+    # Parse offer IDs
+    offers_ids = [int(offer) for offer in offer_ids.split(",")]
+
+    # Sample random offer ID
+    offer_id =  offers_ids[np.random.randint(0, len(offers_ids))]
+
+    # Prepare response
+    response = {
+        "offer_id": offer_id,
+    }
+
+    return response
+
+
+def main() -> None:
+    """Run application"""
+    uvicorn.run("1:app", host="localhost")
+
+
+if __name__ == "__main__":
+    main()

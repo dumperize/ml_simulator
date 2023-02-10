c3b0b7b3d53b3aaae578630b4d067020be53ef32
Elena Nikolaevskaya <ole.elena@gmail.com>
2023-02-10 10:09:07 +0400

    junior-10.2

---
+++ junior/10/2.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+from scipy import stats
+import numpy as np
+
+
+def cpc_sample(
+    n_samples: int, conversion_rate: float, reward_avg: float, reward_std: float
+) -> np.ndarray:
+    """Sample data."""
+    cvr = stats.binom.rvs(1, conversion_rate, size=n_samples)
+    cpa = stats.norm.rvs(loc=reward_avg, scale=reward_std, size=n_samples)
+    return cvr*cpa
+
+def t_test(cpc_a: np.ndarray, cpc_b: np.ndarray, alpha=0.05
+) -> Tuple[bool, float]:
+    """Perform t-test.
+
+    Parameters
+    ----------
+    cpc_a: np.ndarray :
+        first samples    
+    cpc_b: np.ndarray :
+        second samples
+    alpha :
+         (Default value = 0.05)
+
+    Returns
+    -------
+    Tuple[bool, float] :
+        True if difference is significant, False otherwise
+        p-value
+    """
+    ttest = stats.ttest_ind(cpc_a, cpc_b)
+    return (bool(ttest.pvalue < alpha), float(ttest.pvalue))
+
+# cpv1 = cpc_sample(10000, 0.3, 4,2)
+# cpv2 = cpc_sample(10000, 0.3, 4,2)
+# t = t_test(cpv1, cpv2)
+# print(t)
